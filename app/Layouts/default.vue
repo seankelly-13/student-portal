884ba07e3cd1f5cf095c29cbda3a152c4f9248f3
seankelly-13 <seanvistan13@gmail.com>
2026-05-05 09:58:43 +0800

    layouts setup using navigation

---
+++ app/Layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+        color="surface-variant" 
+        v-model="drawer">
+            <v-list>
+                <v-list-item
+                prepend-icon="mdi-view-dashboard"
+                title="Home"
+                value="home"
+            ></v-list-item>
+            <v-list-item
+                prepend-icon="mdi-information-variant"
+                title="About"
+                value="home"
+            ></v-list-item>
+            </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+        <v-app-bar-nav-icon 
+            @click="drawer = !drawer"
+        ></v-app-bar-nav-icon>
+
+        <v-app-bar-title>
+            Student Portal
+        </v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <NuxtPage />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
---
+++ app/Pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-col>
+                        <v-text-field 
+                            label="Last Name" 
+                            variant="solo-inverted"
+                            placeholder="Vistan"
+                            clearable
+                        ></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-btn 
+                            icon="$vuetify" 
+                            variant="tonal"
+                        ></v-btn>
+                    </v-col>
+                    <v-col>
+                        
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
