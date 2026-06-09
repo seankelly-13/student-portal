698c56cc70bf29fe72da134abbbdc39bc7f886eb
Cranch Mangulabnan <iamdummyscholar@gmail.com>
2026-06-09 10:04:44 +0800

    web strapi connection

--- web/app/Pages/index.vue
+++ web/app/Pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-    
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-card>
+                    <v-card-title>
+                        NUXT 4 + STRAPI CONNECTION CHECK
+                    </v-card-title>
+                    <v-card-text>
+                        setup is working: {{ config.public.strapiUrl }}
+                    </v-card-text>
+                </v-card>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -2,4 +2,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   modules: ['vuetify-nuxt-module', '@nuxtjs/strapi'],
+
+  runtimeConfig: {
+    public: {
+      strapiUrl: ''
+    }
+  }
+
+
 })
